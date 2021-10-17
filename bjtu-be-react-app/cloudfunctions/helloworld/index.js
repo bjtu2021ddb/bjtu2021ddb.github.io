b18e44e91980525d872c200ab7d504a2d2673679
bjtu2021ddb <bjtu2021ddb@163.com>
2021-10-17 17:09:15 +0800

    tcbb init

--- bjtu-be-react-app/cloudfunctions/helloworld/index.js
+++ bjtu-be-react-app/cloudfunctions/helloworld/index.js
@@ -1,14 +1,10 @@
 const cloud = require("@cloudbase/node-sdk");
 
 exports.main = async (event, context) => {
-  const app = cloud.init({
-    env: cloud.SYMBOL_CURRENT_ENV,
-  });
+  const app = cloud.init({ env: cloud.SYMBOL_CURRENT_ENV });
 
-  // todo
-  // your code here
   return {
     event,
-    message: 'hello world'
+    message: "helloworld",
   };
 };
